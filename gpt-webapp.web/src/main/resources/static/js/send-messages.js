5a3f57547c0cd24b7a0e41c8f2a39188bb1d49c2
oroiteave <oroiteave@gmail.com>
2024-10-20 14:04:58 -0300

    send test messages in open chats

--- gpt-webapp.web/src/main/resources/static/js/send-messages.js
+++ gpt-webapp.web/src/main/resources/static/js/send-messages.js
@@ -1,12 +1,12 @@
-function sendMessage() {
-    const userMessageInput = document.getElementById('userMessage');
+function sendMessage(chatId) {
+    const userMessageInput = document.getElementById(`chat-input-${chatId}`);
     const messageText = userMessageInput.value.trim();
     const params = new URLSearchParams();
     params.append("message",messageText);
     
     if (messageText !== '') {
         // Agregar el mensaje del usuario al chat
-        addMessageToChat(messageText, 'user');
+        addMessageToChat(messageText, 'user',chatId);
 
         fetch('/messages/test', {
 		method: 'POST',
@@ -17,15 +17,15 @@ function sendMessage() {
 		})
 		.then(response => response.text())
 		.then(data => {
-			addMessageToChat(data, 'bot');
+			addMessageToChat(data, 'bot',chatId);
 		}).catch(error => console.error('Error fetching the test', error));
 
         userMessageInput.value = ''; // Limpiar el campo de texto
     }
 }
 
-function addMessageToChat(message, sender) {
-    const chatMessages = document.getElementById('chatMessages');
+function addMessageToChat(message, sender,chatId) {
+    const chatMessages = document.getElementById(`chat-messages-${chatId}`);
     const messageElement = document.createElement('div');
     messageElement.classList.add('message', sender);
     messageElement.textContent = message;
